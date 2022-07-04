db08010526124c494c54719274c2f9ba3382e430
KayliedeBlois <kayliedeblois@gmail.com>
2022-07-04 14:25:22 -0400

    Added refactored eqArrays and eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual ===  expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++){
@@ -13,14 +7,6 @@ const eqArrays = function(array1, array2) {
     } 
   }
   return true;
-}; 
+};
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
